cefa5d350750e978b680ac82bf8fdd0a08201a38
quangnam <quangnam@gmail.com>
2025-04-13 20:25:27 +0700

    code video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,22 @@
 import './style.css'
 
 const MyComponent = () => {
+    const string = "Hello World!";
+    const number = 20;
+    const boolean = true;
+    const nullValue = null;
+    const array = [1, 2, 3, 4, 5];
+    const object = {
+        name: "Quang Nam",
+        age: 20,
+        address: "Tay Ho"
+    }
     return (
         <>
-            <div className="name">Quang Nam</div>
+            <div>
+                {console.log("Hello World!")}
+            </div>
+            <div className="name">Quang Nam & {number} & {string} &  {boolean} & {nullValue} & {array} & {object} </div>
         </>
     );
 }
