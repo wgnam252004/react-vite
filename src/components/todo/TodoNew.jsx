a467ef63dbbc7c1fbc8e81cc53b22f85b5207e42
quangnam <quangnam@gmail.com>
2025-04-21 10:35:46 +0700

    code video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,11 +2,22 @@ const TodoNew = (props) => {
     console.log(">>> check props", props)
     const { addNewData } = props;
 
+    const handleClick = () => {
+        alert("Click me")
+    }
+
+    const handleOnChange = (event) => {
+        console.log(">>> check onChange", event.target.value)
+    }
     // addNewData("Quang Nam");
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => {handleOnChange(event.target.value)}}
+            />
+            <button style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
