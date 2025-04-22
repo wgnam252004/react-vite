b249b133364ca5674abc143a45d23be603849244
quangnam <quangnam@gmail.com>
2025-04-22 09:51:58 +0700

    code video 39

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,18 +1,26 @@
 const TodoData = (props) => {
 
-  const { name, age, data } = props;
+  const { todoList } = props;
 
-  console.log(">>> check props", props)
-    return (
-        <div className="todo-data">
-          <div>My name is {name}</div>
-          <div>Learning react</div>
-          <div>Watching youtube</div>
+  console.log(">>> check props", todoList)
+  return (
+    <div className="todo-data">
+      {todoList.map((item, index) => {
+        console.log(">>> check map", item, index)
+        return (
           <div>
-            {JSON.stringify(props.todoList)}
+            <div className="todo-item">
+              {item.name}
+              <button>Delete</button>
+            </div>
           </div>
-        </div>
-    )
+        )
+      })}
+      <div>
+        {JSON.stringify(props.todoList)}
+      </div>
+    </div>
+  )
 }
 
 export default TodoData;
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -9,7 +9,7 @@ const TodoNew = (props) => {
 
     const handleClick = () => {
         addNewTodo(valueInput)
-
+        setValueInput("");
     }
 
     const handleOnChange = (name) => {
@@ -22,6 +22,7 @@ const TodoNew = (props) => {
         <div className="todo-new">
             <input type="text"
                 onChange={(event) => { handleOnChange(event.target.value) }}
+                value={valueInput}
             />
             <button style={{ cursor: "pointer" }}
                 onClick={handleClick}
